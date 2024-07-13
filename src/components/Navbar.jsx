2e587bc2c345028f04a4021ee02d0fe5be440945
subir-raju <subir.raju0@gmail.com>
2024-07-14 00:49:55 +0300

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="container nav_bar">
+        <div className="left nav-items">SUBIR"S PAGE</div>
+        <div className="right">
+          <li className="nav-items">Home</li>
+          <li className="nav-items">About</li>
+          <li className="nav-items">Blog</li>
+          <li className="nav-items">Projects</li>
+          <li className="nav-items">Contact</li>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
