cbb8972be6a3327ca47ba193a33afabb5a789c4d
subir-raju <subir.raju0@gmail.com>
2024-07-14 00:42:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+  <>
+  <Navbar />
+  </>
+  );
+}
+
+export default App;
