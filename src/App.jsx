22941a261117db2458429af0872279f997c37401
subir-raju <subir.raju0@gmail.com>
2024-07-14 13:28:19 +0300

    AOS package import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -7,8 +7,13 @@ import Experience from './components/Experience'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+    useEffect(() => {
+      Aos.init();
+    }, []);
   return (
   <>
   <Navbar />
