fca5c8fb1ebbf4fb5652cd5e85f84d0b340e34cf
subir-raju <subir.raju0@gmail.com>
2024-08-03 03:16:59 +0300

    navbar space around fixed

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import blog from '../data/blog.json'
-import { Message, Button, Divider } from "rsuite";
+
+
 
 const Blog = () => {
   return (
@@ -12,23 +13,9 @@ const Blog = () => {
             <>
               <div key={data.id} className="blog-id">
                 <div className="blog-body">
-                  <h2 className="card-title">{data.title}</h2>
-                  <p className="card-text">{data.description}</p>
+                  <h2 className="card-text">{data.description}</h2>
                 </div>
               </div>
-
-              <Message
-                showIcon
-                type="info"
-                header="Do you want to allow notifications?"
-              >
-                <p> We can let you know when new messages arrive.</p>
-                <hr />
-                <ButtonToolbar>
-                  <Button size="sm">Don't allow</Button>
-                  <Button size="sm">Allow</Button>
-                </ButtonToolbar>
-              </Message>
             </>
           ))}
         </div>
